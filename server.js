8316e8fc2d139dd6f61c3d4d7b388f6a984dd646
paulooliveira152012 <paulooliveira152012@gmail.com>
2022-07-01 18:42:28 -0400

    adding party name to results (1 problem --> party 1 is displaying as NULL instead of JS Juggernauts)

--- server.js
+++ server.js
@@ -29,7 +29,11 @@ const db = mysql.createConnection(
 
   //Get all candidates
   app.get('/api/candidates', (req, res) => {
-    const sql = 'SELECT * FROM candidates';
+    const sql =`SELECT candidates.*, parties.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties
+                ON candidates.party_id = parties.id`;
 
     //function to return all data from the database in candidates table
   db.query(sql, (err, rows) => {
@@ -47,7 +51,12 @@ const db = mysql.createConnection(
 
 //   // GET a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql =`SELECT candidates.*, parties.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties
+                ON candidates.party_id = parties.id
+                WHERE candidates.id = ?`;
     const params = [req.params.id];
 
     db.query(sql, params, (err, row) => {

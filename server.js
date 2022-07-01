929d443208d235a88a1d35f81a4fb946c06780e2
paulooliveira152012 <paulooliveira152012@gmail.com>
2022-07-01 14:31:19 -0400

    connecting to the database

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+//connect to MySQL database by importing mysql2 package
+const mysql = require('mysql2');
+
 //import express from node_modules
 const express = require('express');
 //Adding the PORT designation 
@@ -7,6 +10,19 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
 
   // Default response for any other request (Not Found)
 app.use((req, res) => {
